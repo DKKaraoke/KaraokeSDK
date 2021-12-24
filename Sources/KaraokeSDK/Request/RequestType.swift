1b58257fa274deee65fe53b3ee891c5c11018468
tkgstrator <tkgstrator@users.noreply.github.com>
2021-12-24 13:45:23 +0900

    - QRコード読み取りに対応

--- Sources/KaraokeSDK/Request/RequestType.swift
+++ Sources/KaraokeSDK/Request/RequestType.swift
@@ -16,6 +16,7 @@ protocol RequestType {
     var path: String { get }
     var encoding: ParameterEncoding { get }
     var parameters: Parameters { get set }
+    func asURLRequest(cdmNo: String?, qrCode: String?) throws -> URLRequest
 }
 
 extension RequestType {
@@ -27,11 +28,15 @@ extension RequestType {
         JSONEncoding.default
     }
     
-    func asURLRequest(cdmNo: String, qrCode: String) throws -> URLRequest {
+    func asURLRequest(cdmNo: String?, qrCode: String?) throws -> URLRequest {
         var request = URLRequest(url: baseURL.appendingPathComponent(path))
         request.httpMethod = method.rawValue
         request.timeoutInterval = TimeInterval(5)
         
+        guard let cdmNo = cdmNo, let qrCode = qrCode else {
+            throw NSError(domain: "No qrCode or cdmNo", code: 401, userInfo: nil)
+        }
+
         let parameters = parameters.merging(["cdmNo": cdmNo, "qrCode": qrCode]) { $1 }
         return try encoding.encode(request, with: parameters)
     }
